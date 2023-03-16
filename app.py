952e12c0ee2267a430301848db4fc5e927340800
Vishal Pandey <bishalpandey2001@gmail.com>
2023-03-16 12:48:09 +0530

    Update Streamlit

--- app.py
+++ app.py
@@ -3,19 +3,35 @@ import pandas as pd
 import numpy as np
 import pickle
 
-# Load the diabetes dataset
-df = pd.read_csv('NCSU_Dataset.csv')
-
-# Select the features for clustering
-X = df[['Age', 'Pregnancies', 'Glucose']].values
-
-# Load the KMeans model from a pickle file
+# Load the KMeans model from file
 with open('kmeans_model.pkl', 'rb') as f:
     kmeans = pickle.load(f)
 
 # Define a function to predict cluster membership for new data
 def predict_cluster(age, pregnancies, glucose):
     return kmeans.predict([[age, pregnancies, glucose]])[0]
+
+# Define a function to assign cluster conditions
+def get_cluster_conditions(cluster_num, age, pregnancies, glucose):
+    if cluster_num == 0:
+        if age >= 13 and age <= 18 and (glucose <= 120 or glucose >= 170):
+            return "Type 1 Diabetes"
+        else:
+            return "Type 1 Diabetes"
+    elif cluster_num == 1:
+        if age >= 18 and age <= 30 and (glucose <= 90 or glucose >= 130):
+            return "Type 2 Diabetes"
+        elif age >= 30 and (glucose <= 90 or glucose >= 130):
+            return "Type 2 Diabetes"
+        else:
+            return "Type 1 Diabetes"
+    else:
+        if age >= 18 and age <= 30 and pregnancies == 1 and (glucose <= 90 or glucose >= 130):
+            return "Type 3 Diabetes"
+        elif age >= 30 and pregnancies == 1 and (glucose <= 90 or glucose >= 130):
+            return "Type 3 Diabetes"
+        else:
+            return "Type 2 Diabetes"
 
 # Create the Streamlit web app
 st.title('Diabetes Cluster Prediction')
@@ -25,6 +41,7 @@ age = st.slider('Age', min_value=0, max_value=100, step=1)
 pregnancies = st.slider('Number of Pregnancies', min_value=0, max_value=20, step=1)
 glucose = st.slider('Glucose Level', min_value=0, max_value=200, step=1)
 
-# Make a prediction and display the predicted cluster number
+# Make a prediction and display the predicted cluster condition
 cluster_number = predict_cluster(age, pregnancies, glucose)
-st.write('The predicted cluster number is', cluster_number)
+cluster_condition = get_cluster_conditions(cluster_number, age, pregnancies, glucose)
+st.write('The predicted cluster condition is:', cluster_condition)
